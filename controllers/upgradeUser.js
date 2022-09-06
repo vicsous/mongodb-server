c1a89eb54ab1d05e92706f9d5cea02a8bf1f8425
vicsous <vicsous@protonmail.com>
2022-09-05 21:49:39 -0300

    Setting json response

--- controllers/upgradeUser.js
+++ controllers/upgradeUser.js
@@ -3,7 +3,7 @@ const { User } = require('../db/models');
 
 const upgradeUser = async (req, res) => {
     const user = await User.find({ username: req.params.username.toUpperCase() });
-    if (!user[0]) return res.status(404).json({ message: 'User not found' });
+    if (!user[0]) return res.status(200).json({ message: 'User not found' });
     req.body.upgrade ? roles = [ process.env.USER_CODE, process.env.MOD_CODE ] : roles = [ process.env.USER_CODE ]; 
     const { status, data } = await updateFile(req.params.username.toUpperCase(), { roles }, User);
     return res.status(status).json(data);
